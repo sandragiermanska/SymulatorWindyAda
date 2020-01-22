f6c28dd35d8d69436ce6ff530fbb75e49df37e0c
kkoziol72 <33664563+kkoziol72@users.noreply.github.com>
2020-01-22 03:05:30 +0100

    Create test6.adb

---
+++ test6.adb
@@ -0,0 +1,11 @@
+with winda, Ada.Text_IO;
+use Ada.Text_IO;
+
+procedure Test6 is
+begin
+    Put_Line("Testowanie symulatora ruchu windy");
+    Put_Line("Test 6: 1 -> 2 -> 3 -> 4 -> 5 -> 7 -> 8");
+    Test6;
+    Winda1;
+    Put_Line("");
+end Test6;
